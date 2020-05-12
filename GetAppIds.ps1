c86e9c8e3cdf584f1118d88c416f8638e7c8e688
Rainier-MSFT <rainieruk@live.co.uk>
2020-05-12 22:46:01 +0100

    Add files via upload

---
+++ GetAppIds.ps1
@@ -0,0 +1,36 @@
+﻿#$Apps = import-csv -Path "C:\SHA\f5apps.csv"
+#$Apps = "82c12eeb-486d-4327-9a29-0735967cde7c","641513e7-9f0c-4b9f-a2d4-6d5c27b7d210","e50a0222-a49a-4132-adf3-4567f621df84","4067f128-0fd8-48e5-a6ca-dba543118ab6","69f364cb-8cc1-4eda-b1fc-3dd7e222a03f","66513d4a-8fdf-4f72-b0af-68d0bbf8fd45","1013532d-c061-41af-a6b7-e97eab1ee8bf","c01c698b-ed94-4a27-9a9d-a3c35f3893f0","d81e6389-e43b-4bcf-9a3a-8c62c227b1ca","a920824c-5066-4816-9a78-729b5f1883ca","eaa9dc76-e7a1-4630-8d2c-4743100dd226","2458071f-b742-4998-b978-3463176bc99f","6efc418e-7025-4a26-a1e2-f9264d1ed420","b8f13087-c3fb-4610-bada-9481650c643c","e65f5bc0-02da-4688-810f-9c45f4b8062f","76a1def8-d5b3-46e8-93f5-2fc6093dabbc","80b8636c-3664-4b7e-85ad-36052787ec7f","1bf6692a-cea9-454e-9164-752044955c3a","c22bdb0d-512a-4498-89e2-68a8ef11afc5","c5551798-fd48-4618-a55f-8cf2c492ad5a","44ea6fd3-9e60-4e4c-a617-2dcb892ab93d","2bee749e-7e7c-47f0-b291-337416860fcf","76722096-4a15-4e18-a73a-cfb0e7275494","2488b41e-c83d-436c-a988-6e837f7d6792","e4cd2273-7d05-4d20-ace4-0c4bb567b3cf","f519e47e-1a12-4fd5-a53d-fb792b6c02a3","6a863364-a029-4525-a411-2f074a950f2e","ac0a34b1-07a4-4992-a230-a2bfd8580369","239c1037-da59-46ca-b213-d2aafced063b","fbceda1d-6bde-43e5-82dd-032952974c1e","4d009dfa-97ec-431d-b297-95a8ac2571be","e65bddb1-0976-485f-abeb-1a2ae7d8b583","3adbc972-8770-4b7e-8e4c-f0977b4ea8da","5c7a9dd5-750a-4b49-b80e-68823fdf3fb6","1c633f1c-f219-4d27-9802-45905d5db066","cfc6be80-faac-4075-bd1c-ba7bbeb756e0","d7c323a1-a95f-4d97-9b63-6e2d8e702e40","f191751f-4179-4083-92e7-f19088a51334","def28bbf-75bb-429f-95a4-dcc73f749242","6ec44d96-b742-4f6f-9915-fa0c4145ea66","634cb25c-57f0-430f-a567-a1020a548203","5db78e9f-22cb-4c09-9a5f-24a33d2f434d","638671ce-d228-457c-9045-62970ca45fa0","06c421d1-9233-4e78-b655-7ee711eaa84e","816f19ad-9ee6-4356-ba4b-6948fb2846b6","72493936-0104-428f-8f1e-894f05645240","7c30ee65-94c4-4a56-8988-f050e0f41980","2aca98c1-651e-4cc0-8d22-fbaa32ad5ff4","cd57059f-5752-4bd8-b355-9ad793c84b47","e759637c-d4b2-4bb0-820f-7a1412c35025","990fc8c4-6e17-46af-b38c-0b3d41ee98f6","124f9803-8477-4c61-8570-40f5fc306167","b97f8933-972e-4d5c-a30d-a7b7adf2dcf4","5f3eb63f-e9b8-4db5-9f1f-f3a0ed3e32df","92e0c5a7-18af-4a85-8bb1-9934c460bb01","38632598-895c-429b-bcbb-970d1e9fa22d","0e032bf5-59b9-4f21-bd7f-3b3823034b25","577c4ea0-cc15-4458-ae3f-a0ba23866631","33720040-e207-483f-bbf0-009acfba6061","d13f379e-b2f4-4f2f-ab76-92c40137d91e","c5b4b6cb-b323-4456-9ac3-46139f961f8a","37ad4d20-3b47-4c0b-9e18-bf5371ac7da3","c44515bf-56c6-4431-b179-5d90cd99928f","f35876e4-1931-4cc8-abd4-7bdd91130d22","b88f3ce0-0ab4-4cc4-9d07-3c217b686fef","ec234fc6-9ffd-4c16-9a5d-a905ac566ade","8120ee4d-917c-48f5-9f21-1eb82542f070","86c13a7c-ed15-4285-b4e4-a1b9da411cb5","06bf5f26-5893-45dc-8057-eb589607e654","dbbd35a2-bbba-4952-a58e-840fd623caa9","f7e73d9f-4cf0-4adf-828b-ef291d64ac1c","cf97716d-285b-4609-8c52-b1c51d3c697a","c89394cb-6b25-49bf-b664-beb292c6bab3","7ee79962-2b69-4e64-a060-995b03a9a955","e402e13f-5c98-4434-8af3-1436b03c0d72","376ec41a-b234-4f7b-a935-c96d30cd1897","e4d65382-ada2-42e1-b364-6d4158d15233","1bb4fd73-cb50-4576-9cb4-dfc5b8a39f15","d9068b16-e54a-4d13-a386-0159e6661604","2e79e99a-7b6b-49cf-93a9-9872a27b23b4","274d81a9-184b-4537-9d4b-5c70053b6258","4caeaff5-17a9-4265-a22a-be502682646f","e5e5a248-eefe-4528-ad26-a40fbb5e5050","b9f0aa79-6cb0-46a9-94f0-e8a2791715fb","b03740c8-c7dd-497c-82d8-b54de5b2d296","fed56bd6-4ba6-47fc-b3de-1ccc6035c343","6887339f-3e86-492b-8012-920923ee5b22","8e60a354-21d1-428e-be7d-73e9838c75b6","65800f49-c381-490f-85fd-1e27d9e54360","5b5c8101-b5e1-4feb-a28a-6e73c01ca6c8","f9579539-5800-40ef-8250-31b5052bc145","17eb171a-08a3-416d-977e-f9960ac3b11c","405e1603-1b6f-4e26-8987-ad5b41130d4b","9ca399ac-f098-4c50-8889-ed3e3b8ce6d2","2176b4b1-abff-4f3d-8fae-c4b051dc091f","d70e68fd-b783-4830-a1d0-1936c8ce64d1","0d80aa57-05cd-4917-918d-84ff60a50320","230e24ef-96c9-4f65-8a5e-4a580addb55c","f46bc9cb-6f64-47d0-b5de-d9d8652be44c","a57b9553-510a-4417-93e2-872a5360fee0","6110796c-09d9-4dd0-904e-49982bdf86d6","d9154333-e83b-47ff-ae1d-46ebe6f2b463","e0b16905-c301-40dd-b97c-8ac53045d1e2","2f49d19b-4e94-492b-9b66-e92262dc7b6a","2b4d03e5-1975-4937-b628-de9649d1926a","168dae70-de32-4466-931c-4b2ff3b4afcc","bbb775e8-29f4-4614-a832-5786eb8f4d75","ad95cd3e-b597-4384-81db-3ae1ef65f5ae","ab3efe80-3876-4886-92c5-246061847f40","5a4d690a-3c57-45ec-a432-341b4a4a87cb","ac9a835e-fa90-477b-89c6-7dfb85f5551c","9fee308c-7ab5-4f41-9b50-c3b4feae00e8","f2a90bf4-ff70-46d8-94db-e8c6341afc81","bd5dc1d9-7f24-4af7-871b-bcf2429156f2","31f4996e-4e80-4e40-bfc2-b3fcfded268b","e07db328-f440-4dff-aa34-18ca0f623655","957e3a87-450d-4ea1-9aee-9f389a29ca13","3122e90e-e0da-4ec4-ad44-708c5c24d470","c46c3d67-e876-409f-b53b-5a4459579314","d3baabe4-b48b-41c9-9db6-d6964698e996","27a09d4a-7df9-4c96-af2b-821b2901316a","e256317e-2ba8-489e-aafc-92d2658729a1","173e6709-ad57-4f27-a45c-33f77d07c745"
+
+# Get tenant details to test that Connect-AzureAD has been called
+try {
+    $tenant_details = Get-AzureADTenantDetail
+} catch {
+    throw "You must call Connect-AzureAD before running this script."
+}
+Write-Verbose ("TenantId: {0}, InitialDomain: {1}" -f `
+                $tenant_details.ObjectId, `
+                ($tenant_details.VerifiedDomains | Where-Object { $_.Initial }).Name)
+
+$i = 1
+$App_List = @()
+$count = $apps.count
+Foreach ($app in $apps)
+{
+    Write-Progress -Activity "Getting $($app.DisplayName) App information" -Status "$i of $count" -PercentComplete ($i/$count*100)
+      try {
+            $f5apps = Get-AzureADApplication -Filter "AppId eq '$app'" -ErrorAction Stop
+            $result = " " | Select-Object DisplayName, ObjectId, AppId, HomePage, ReplyUrls
+            $result.DisplayName = $($f5apps.Displayname)
+            $result.ObjectId = $($f5apps.ObjectId)
+            $result.AppId = $($f5apps.AppId)
+            $result.HomePage = $($f5apps.HomePage)
+            $result.ReplyUrls = $($f5apps.ReplyUrls)
+            $App_List += $result
+          }
+      catch {   
+             $Problem = $PSItem.exception.message         
+            }
+    $i++
+}
+
+$App_List | Out-GridView
